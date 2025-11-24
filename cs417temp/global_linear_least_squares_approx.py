b9d4df1908112d72225cee28887877efe387fcff
Emily Rick <emrick.2261@gmail.com>
2025-11-24 13:48:20 -0500

    started the linear least squares approximation module

---
+++ cs417temp/global_linear_least_squares_approx.py
@@ -0,0 +1,12 @@
+"""
+This is the start of the global Linear Least Squares Approximation module.
+Take one of the Discrete Least Squares Approximation methods below and incorporate
+it into the codebase. Only implement one of the approximation methods.
+
+Discrete Least Squares Approximation methods:
+1. XTX|XTY method
+
+2. A|b⃗  method
+
+3. Pre-solved version of the A|b⃗  method
+"""
